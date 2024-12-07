3a9d5d39aa14653cbbbc5c9e843efc15496f6e5f
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2024-12-07 18:49:36 +0100

    Relacionar modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function rol(): BelongsTo{
+        return $this->belongsTo(Rol::class);
+    }
 
     /**
      * The attributes that are mass assignable.
