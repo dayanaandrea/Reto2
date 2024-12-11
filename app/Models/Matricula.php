62182c61f8e008f682e5ba280bc321c881b82ee3
Dayana <elorrieta@104-04.elorrieta.int>
2024-12-11 17:17:01 +0100

    Se modifica matricula model y reunion seeder

--- app/Models/Matricula.php
+++ app/Models/Matricula.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Matricula extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_estudiante');
+    }
+
+    public function modulo()
+    {
+        return $this->hasMany(Oferta::class, 'id_modulo');
+    }
 }
--- database/seeders/ReunionSeeder.php
+++ database/seeders/ReunionSeeder.php
@@ -15,7 +15,7 @@ class ReunionSeeder extends Seeder
     {
 
         DB::table('reuniones')->insert([
-            ['fecha' => now()->toDateString(), 'hora'=> '15:30:00', 'estado'=> 'pendiente','id_profesor'=> 1,'id_estudiante'=> 25, 'created_at'=>now(),'updated_at'=>now()],
+            ['fecha' => now()->toDateString(), 'hora'=> '15:30:00', 'estado'=> 'pendiente','id_profesor'=> 10,'id_estudiante'=> 25, 'created_at'=>now(),'updated_at'=>now()],
             ['fecha' => now()->toDateString(), 'hora'=> '09:30:00', 'estado'=> 'aceptada','id_profesor'=> 15,'id_estudiante'=> 30, 'created_at'=>now(),'updated_at'=>now()],
             ['fecha' => now()->toDateString(), 'hora'=> '10:30:00', 'estado'=> 'rechazada','id_profesor'=> 20,'id_estudiante'=> 50, 'created_at'=>now(),'updated_at'=>now()],
         ]);
