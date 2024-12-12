20b63f7785e4b108bbd2ceb95cb4b56181718bf4
Leire Lasa <leire.lasaam@elorrieta-errekamari.com>
2024-12-12 17:26:18 +0100

    Eliminar home_

--- resources/views/home_estudiante.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h2>Home de estudiante</h2>
-    <p>Bienvenido {{$user->nombre}} {{$user->apellidos}}</p>
-</div>
-@endsection
