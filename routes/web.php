62975521f8308694f63946e134fb786f3ab6ce9f
LucianJZN <lucianstelian.grecianu@elorrieta-errekamari.com>
2024-12-12 19:58:01 +0100

    Agregado student controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Auth;
 
 Route::permanentRedirect('/', '/home');
@@ -12,6 +13,8 @@ Auth::routes();
 Route::middleware(['auth'])->group(function () {
     Route::resources([
         'home' => HomeController::class,
-        'admin' => AdminController::class
+        'admin' => AdminController::class,
+        'student' => StudentController::class
+        
       ]);
 });
