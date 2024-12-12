e08c99a2380d662fbaf6f064ac6701831da9a4b2
LucianJZN <lucianstelian.grecianu@elorrieta-errekamari.com>
2024-12-12 16:53:50 +0100

    arreglado el DatabaseSeeder que puebla la BBDD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,11 @@ class DatabaseSeeder extends Seeder
         $this->call(RolSeeder::class);
         // Insertar los usuarios
         $this->call(UserSeeder::class);
-        $this->call(CicloSeeder::class);
-        $this->call(ModuloSeeder::class);
-        $this->call(OfertaSeeder::class);
-        $this->call(ReunionSeeder::class);
-        $this->call(MatriculaSeeder::class);
+        $this->call(CycleSeeder::class);
+        $this->call(ModuleSeeder::class);
+        $this->call(OfferSeeder::class);
+        $this->call(MeetingSeeder::class);
+        $this->call(EnrollmentSeeder::class);
 
     }
 }
