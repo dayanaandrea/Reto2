630553f202db5cef5e12f5c6d18e3f1cd6550742
Dayana <dayana@DESKTOP-91VHDON.>
2025-01-02 12:51:24 +0100

    Se añade, ver, eliminar y crear y se soluciona problema

--- resources/views/admin/module/success.blade.php
+++ resources/views/admin/module/success.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-4">
     <div class="alert alert-success" role="alert">
         <h4 class="alert-heading">Eliminación exitosa</h4>
-        <p>El usuario <b>{{ $email }}</b> ha sido eliminado correctamente.</p>
+        <p>El modulo <b>{{ $name }}</b> ha sido eliminado correctamente.</p>
         <hr>
         <p class="mb-0">¡La acción se completó de manera satisfactoria!</p>
     </div>
