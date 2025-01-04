0c4eb409ca2db536d85f21143d11256011ff2482
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-01-04 17:08:47 +0100

    Gestionar error en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,38 +1,40 @@
 @extends('layouts.app')
 
 @php
-$user = Auth::user();
+    $user = Auth::user();
 @endphp
 
 @section('content')
 <div class="container">
-    @if ($user->role->role == 'profesor')
-    <h2 class="mb-3">Profesores</h2>
-    <table class="table table-striped">
-        <thead>
-            <tr class="table-dark text-uppercase">
-                <th scope="col">#</th>
-                <th scope="col">Apellidos</th>
-                <th scope="col">Nombre</th>
-                <th scope="col">Correo</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($teachers as $teacher)
-            <tr>
-                <th scope="col">{{ $loop->iteration }}</th>
-                <td>{{$teacher->lastname}}</td>
-                <td>{{$teacher->name}}</td>
-                <td>{{$teacher->email}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    @elseif ($user->role->role == 'estudiante')
-    <div class="container">
-        <h2>Home de estudiante</h2>
-        <p>Bienvenido {{ $user->name }} {{ $user->lastname }}</p>
-    </div>
+    @if($user->role)
+        @if ($user->role->role == 'profesor')
+            <h2 class="mb-3">Profesores</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr class="table-dark text-uppercase">
+                        <th scope="col">#</th>
+                        <th scope="col">Apellidos</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Correo</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($teachers as $teacher)
+                        <tr>
+                            <th scope="col">{{ $loop->iteration }}</th>
+                            <td>{{$teacher->lastname}}</td>
+                            <td>{{$teacher->name}}</td>
+                            <td>{{$teacher->email}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @elseif ($user->role->role == 'estudiante')
+            <div class="container">
+                <h2>Home de estudiante</h2>
+                <p>Bienvenido {{ $user->name }} {{ $user->lastname }}</p>
+            </div>
+        @endif
     @endif
 </div>
 @endsection
