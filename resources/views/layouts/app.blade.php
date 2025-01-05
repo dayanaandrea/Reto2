bca30ab2d179151ca222056254fe7101debe499d
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-01-05 12:48:52 +0100

    Incluir logo en la barra

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,16 +22,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <!-- Poner un enlace según el rol del usuario -->
-                @if (Auth::user() && (Auth::user()->role->role == 'god' || Auth::user()->role->role == 'administrador'))
-                    <a class="navbar-brand" href="{{ url('/admin') }}">
-                        {{ config('app.name', 'ElorAdmin') }}
-                    </a>
-                @else
-                    <a class="navbar-brand" href="{{ url('/home') }}">
-                        {{ config('app.name', 'ElorAdmin') }}
-                    </a>
-                @endif
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    <img src="{{Storage::url('public/images/logo-elorrieta.svg')}}" alt="logo" class="img-fluid" style="max-width: 150px;">
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -67,36 +60,36 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <img src="{{ obtenerFoto(Auth::user()) }}" alt="usuario"
-                                        class="img-fluid rounded-circle mx-2" style="max-width: 30px; max-height: 30px;">
-                                    {{ Auth::user()->name }}
-                                </a>
+                                                <li class="nav-item dropdown">
+                                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                                        <img src="{{ obtenerFoto(Auth::user()) }}" alt="usuario"
+                                                            class="img-fluid rounded-circle mx-2" style="max-width: 30px; max-height: 30px;">
+                                                        {{ Auth::user()->name }}
+                                                    </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    @php
-                                        if (Auth::user() && (Auth::user()->role->role == 'god' || Auth::user()->role->role == 'administrador')){
-                                            $perfil_ruta = route('admin.users.show', Auth::user());
-                                        } else {
-                                            $perfil_ruta = route('users.show', Auth::user());
-                                        }
-                                    @endphp
-                                    <a class="dropdown-item" href="{{$perfil_ruta}}">
-                                        Perfil
-                                    </a>
+                                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                                        @php
+                                                            if (Auth::user() && (Auth::user()->role->role == 'god' || Auth::user()->role->role == 'administrador')) {
+                                                                $perfil_ruta = route('admin.users.show', Auth::user());
+                                                            } else {
+                                                                $perfil_ruta = route('users.show', Auth::user());
+                                                            }
+                                                        @endphp
+                                                        <a class="dropdown-item" href="{{$perfil_ruta}}">
+                                                            Perfil
+                                                        </a>
 
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                        {{ __('nav.logout') }}
-                                    </a>
+                                                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                                     document.getElementById('logout-form').submit();">
+                                                            {{ __('nav.logout') }}
+                                                        </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                            @csrf
+                                                        </form>
+                                                    </div>
+                                                </li>
                         @endguest
                     </ul>
                 </div>
