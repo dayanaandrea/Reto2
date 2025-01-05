a6f3f545e01a4293b995cbc107a0086b7a1a70d2
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-01-05 15:19:32 +0100

    No enlazar rol si no es god o admin, que son quienes pueden verlos

--- resources/views/components/detail.blade.php
+++ resources/views/components/detail.blade.php
@@ -1,5 +1,5 @@
 <p class="col-sm-3 fw-bold d-inline">{{ $label }}</p>
-@if($route)
+@if($route && Auth::user()->role && (Auth::user()->role->role == 'god' || Auth::user()->role->role == 'administrador'))
 <a href="{{$route}}" class="col-sm-9 d-inline">
     <p class="d-inline">{!! $value !!}</p>
 </a>
