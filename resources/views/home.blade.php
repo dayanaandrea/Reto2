a121a6ee1f6cc610217c07d966a7d803d528e0a9
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-01-05 20:42:51 +0100

    Añadir carrusel de imágenes en home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,48 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
+    <div id="carousel" class="carousel slide mb-5">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true"
+                aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{Storage::url('public/images/estudiantes.jpg')}}" class="d-block w-100 img-fluid"
+                    alt="estudiantes">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>¿Qué es lo siguiente?</h5>
+                    <p>ElorAdmin te ayuda a planear tu semana.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{Storage::url('public/images/programacion.jpg')}}" class="d-block w-100 img-fluid"
+                    alt="programacion">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>¡Empieza a programar!</h5>
+                    <p>Elorrieta-Errekamari ofrece diferentes ciclos para aprender a programar.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{Storage::url('public/images/lab.jpg')}}" class="d-block w-100 img-fluid" alt="laboratorio">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>¿Te gusta la ciencia?</h5>
+                    <p>En Elorrieta-Errekamari puedes convertirte en Técnico de Laboratorio.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
     @if($user->role)
         @if ($user->role->role == 'profesor')
             <h2 class="mb-3">Profesores</h2>
