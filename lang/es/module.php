6adfaebfe843b67ad6167629bf5b9e4037ebf8e8
Dayana <elorrieta@104-04.elorrieta.int>
2025-01-10 19:46:50 +0100

    Se crean documentos para la internalización

---
+++ lang/es/module.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    // INDEX
+    'index_title_1' => 'Crear un nuevo modulo',
+    'index_title_2' => 'Accede a la creación de un modulo :',
+    'module' => 'Modulos',
+    'code' => 'Codigo',
+    'name' => 'Nombre',
+    'hours' => 'Horas',
+    'course' => 'Curso',
+    'cycle' => 'Ciclo',
+    'teacher' => 'Profesor',
+    'actions' => 'Acciones',
+    'not_assigned' => 'No asignado',
+    'see_data_module'=>'Ver datos del modulo',
+    'edit_data_module'=>'Editar datos del modulo',
+    'delete_module'=>'Eliminar modulo',
+    'confirm_deletes'=>'Confirmar eliminación',
+    'confirm_1'=>'¿Estás seguro de que deseas eliminar el módulo ',
+    'confirm_2'=>'? Esta acción no se puede deshacer.',
+    'cancel'=>'Cancelar',
+    'delete'=>'Eliminar',
+
+    // SHOW
+    'show_title' => 'Detalles del modulo',
+    'code' => 'Crear un nuevo modulo',
+    'hours' => '',
+    'course' => '',
+    'cycle' => '',
+    'teacher' => '',
+    'not_assigned' => 'No asignado',
+    // CREATE-EDIT
+   
+];
+
