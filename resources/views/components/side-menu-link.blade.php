c8af91b9377458dde81dc70ce104385bcfde74d9
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-01-11 11:40:23 +0100

    Cambiar estilos en side menu

--- resources/views/components/side-menu-link.blade.php
+++ resources/views/components/side-menu-link.blade.php
@@ -1,5 +1,5 @@
-<li class="nav-item">
-    <a href="{{ $route }}" class="fs-5 mb-2 nav-link text-white {{ Request::is($pattern) ? 'active' : ''}}">
+<li class="nav-item text-small">
+    <a href="{{ $route }}" class="mb-2 nav-link text-white {{ Request::is($pattern) ? 'active' : ''}}">
         <i class="{{$icon}} d-inline-block me-3"></i><span>{{$text}}</span>
     </a>
 </li>
