9bc9afd4279ad16455afd62b914243faf65980d6
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-01-11 14:02:13 +0100

    Pasar lang selector a layout

--- resources/views/layouts/side-menu.blade.php
+++ resources/views/layouts/side-menu.blade.php
@@ -30,15 +30,7 @@
         </a>
         <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser1">
             <li class="dropdown dropend">
-                <a href="#" class="dropdown-item dropdown-toggle" id="dropdownLanguage" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    Idioma
-                </a>
-                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownLanguage">
-                    <li><a class="dropdown-item" href="{{ route('lang', ['locale' => 'en']) }}">English</a></li>
-                    <li><a class="dropdown-item" href="{{ route('lang', ['locale' => 'es']) }}">Español</a></li>
-                    <li><a class="dropdown-item" href="{{ route('lang', ['locale' => 'eus']) }}">Euskera</a></li>
-                </ul>
+                @include('layouts.lang-selector-admin')
             </li>
             <li>
                 @php
