6c799d43c1906e94534b7d6baa97fc154803448c
LucianJZN <lucianstelian.grecianu@elorrieta-errekamari.com>
2025-01-16 17:32:32 +0100

    Crear componente para mostrar los ciclos a los alumnos.

--- app/View/Components/Accordions/CyclesModules.php
+++ app/View/Components/Accordions/CyclesModules.php
@@ -14,6 +14,7 @@ class CyclesModules extends Component
      */
     public function __construct(
         public Collection $cycles
+        
     )
     {
         //
--- resources/views/open/home.blade.php
+++ resources/views/open/home.blade.php
@@ -1,96 +1,171 @@
 @extends('layouts.app')
 
 @php
-$user = Auth::user();
+    $user = Auth::user();
 @endphp
 
 @section('content')
 <div class="container">
     @if(session('permission'))
-    <div class="alert alert-warning alert-dismissible fade show" role="alert">
-        {{ session('permission') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            {{ session('permission') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
     @endif
     @if($user->role->role == 'estudiante')
-    <div id="carousel" class="carousel slide mb-5">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true"
-                aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <div id="carousel" class="carousel slide mb-5">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true"
+                    aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{Storage::url('public/images/estudiantes.jpg')}}" class="d-block w-100 img-fluid"
+                        alt="estudiantes">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>¿Qué es lo siguiente?</h5>
+                        <p>ElorAdmin te ayuda a planear tu semana.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{Storage::url('public/images/programacion.jpg')}}" class="d-block w-100 img-fluid"
+                        alt="programacion">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>¡Empieza a programar!</h5>
+                        <p>Elorrieta-Errekamari ofrece diferentes ciclos para aprender a programar.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{Storage::url('public/images/lab.jpg')}}" class="d-block w-100 img-fluid" alt="laboratorio">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>¿Te gusta la ciencia?</h5>
+                        <p>En Elorrieta-Errekamari puedes convertirte en Técnico de Laboratorio.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{Storage::url('public/images/estudiantes.jpg')}}" class="d-block w-100 img-fluid"
-                    alt="estudiantes">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>¿Qué es lo siguiente?</h5>
-                    <p>ElorAdmin te ayuda a planear tu semana.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="{{Storage::url('public/images/programacion.jpg')}}" class="d-block w-100 img-fluid"
-                    alt="programacion">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>¡Empieza a programar!</h5>
-                    <p>Elorrieta-Errekamari ofrece diferentes ciclos para aprender a programar.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="{{Storage::url('public/images/lab.jpg')}}" class="d-block w-100 img-fluid" alt="laboratorio">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>¿Te gusta la ciencia?</h5>
-                    <p>En Elorrieta-Errekamari puedes convertirte en Técnico de Laboratorio.</p>
-                </div>
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
     @endif
 
     @if($user->role && ($user->role->role == 'profesor' || $user->role->role == 'estudiante'))
-    @php
-    if ($user->role->role == 'profesor') {
-    $schedules = $user->teacherSchedules;
-    } else {
-    $schedules = $user->studentSchedules;
-    }
-    @endphp
+        @php
+            if ($user->role->role == 'profesor') {
+                $schedules = $user->teacherSchedules;
+            } else {
+                $schedules = $user->studentSchedules;
+            }
+        @endphp
 
-    @if(!$schedules->isEmpty())
-    <div class="mb-5">
-        <h2 class="mb-3">Horario</h2>
-        <x-tables.schedule :schedules="$schedules" />
-    </div>
-    @endif
+        @if(!$schedules->isEmpty())
+            <div class="mb-5">
+                <h2 class="mb-3">Horario</h2>
+                <x-tables.schedule :schedules="$schedules" />
+            </div>
+        @endif
     @endif
 
 
     @if($user->role && $user->role->role == 'profesor' && $user->modules)
-    <div class="mb-2">
-        <h2>Módulos</h2>
-        <x-accordions.modules-students :modules="$user->modules" />
-    </div>
+        <div class="mb-2">
+            <h2>Módulos</h2>
+            <x-accordions.modules-students :modules="$user->modules" />
+        </div>
     @endif
 
     @if($user->role)
-    @if ($user->role->role == 'estudiante')
-    <div class="container">
-        <h2>Home de estudiante</h2>
-        <p>Bienvenido {{ $user->name }} {{ $user->lastname }}</p>
-    </div>
-    @endif
+            @if ($user->role->role == 'estudiante')
+                    <div class="container">
+                        <h2>Home de estudiante</h2>
+                        <p>Bienvenido {{ $user->name }} {{ $user->lastname }}</p>
+
+                        <p>Hola</p>
+                        <div class="container">
+                            <h2 class="mb-3">Ciclos</h2>
+
+                            @if($user->enrollments)
+                                            @php
+                                                // Obtener los ciclos únicos, porque sino se printean de forma repetida
+                                                //$uniqueCycles = $user->enrollments->pluck('module.cycle.name')->unique();
+                                                $cycles = $user->enrollments->pluck('module.cycle')->unique('id');
+
+                                            @endphp
+                                <div class="container">
+                                                <h2 class="mb-3">Ciclos</h2>
+                                                @php
+                                                dd($cycle[1]->name);
+                                                @endphp
+                                                <x-accordions.cycles-modules :cycles="$cycles" />
+                                            </div>
+                                            <div class="accordion" id="accordionExample">
+                                                @foreach ($cycles as $cycle)
+                                                    {{ $cycle->name }}
+                                                    <div class="accordion-item">
+                                                        <h2 class="accordion-header">
+                                                            <button class="accordion-button fs-5" type="button" data-bs-toggle="collapse"
+                                                                data-bs-target="#collapse{{$cycle->id}}" aria-expanded="true"
+                                                                aria-controls="collapse{{$cycle->id}}">
+                                                                {{$cycle->name}} ({{$cycle->code}})
+                                                            </button>
+                                                        </h2>
+                                                        <div id="collapse{{$cycle->id}}" class="accordion-collapse collapse show">
+                                                            <div class="accordion-body">
+                                                                <div class="container mb-4">
+                                                                    <h5>Primer Curso</h5>
+
+                                                                    <ol class="list-group list-group-numbered">
+                                                                        @foreach ($cycle->modules as $module)
+                                                                            @if($module->course == 1)
+                                                                                <li class="list-group-item d-flex justify-content-between align-items-start">
+                                                                                    <div class="ms-2 me-auto">
+                                                                                        <div class="fw-bold">{{$module->name}} ({{$module->code}})</div>
+                                                                                    </div>
+                                                                                </li>
+                                                                            @endif
+                                                                        @endforeach
+                                                                    </ol>
+                                                                </div>
+                                                                <div class="container">
+                                                                    <h5>Segundo Curso</h5>
+                                                                    <ol class="list-group list-group-numbered">
+                                                                        @foreach ($cycle->modules as $module)
+                                                                            @if($module->course == 2)
+                                                                                <li class="list-group-item d-flex justify-content-between align-items-start">
+                                                                                    <div class="ms-2 me-auto">
+                                                                                        <div class="fw-bold">{{$module->name}} ({{$module->code}})</div>
+                                                                                    </div>
+                                                                                </li>
+                                                                            @endif
+                                                                        @endforeach
+                                                                    </ol>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                            @else
+                                Sin matrícula
+                            @endif
+
+
+                        </div>
+
+
+
+                    </div>
+            @endif
     @endif
 
-    
+
 
 </div>
 @endsection
