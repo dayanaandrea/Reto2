389d4dbe1017833b5506cc7b1c9501fdb80a2e97
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-02-02 16:54:27 +0100

    Actualizar procedure TeacherSchedule

--- database/migrations/2025_01_27_175511_create_teacher_schedule_procedure.php
+++ database/migrations/2025_01_27_175511_create_teacher_schedule_procedure.php
@@ -13,30 +13,27 @@ return new class extends Migration
     {
         DB::unprepared('
         DROP PROCEDURE IF EXISTS TeacherSchedule;
-            CREATE PROCEDURE TeacherSchedule(
-                IN teacher_id INT,
-                IN selected_week INT
-            )
-            BEGIN
-                select m.code as event, s.day as day, s.hour as hour, "module" as type, null as status, null as meeting_id
-                from users u 
-                    inner join modules m on m.user_id = u.id
-                    inner join schedules s on s.module_id = m.id
-                where u.id = teacher_id
-            UNION
-                select "Reunión" as event, m.day as day, m.time as hour, "own_meeting" as type, m.status as status, m.id as meeting_id
-                from users u 
-                    inner join meetings m on m.user_id = u.id
-                where u.id = teacher_id and m.week = selected_week
-            UNION
-                select "Reunión" as event, m.day as day, m.time as hour, "invited_meeting" as type, m.status as status, m.id as meeting_id
-                from users u 
-                    inner join participants p on p.user_id = u.id
-                    inner join meetings m on m.id = p.meeting_id
-                where u.id = teacher_id and m.week = selected_week
+        CREATE PROCEDURE TeacherSchedule(
+            IN teacher_id INT,
+            IN selected_week INT
+        )
+        BEGIN
+            select m.code as event, s.day as day, s.hour as hour, "module" as type, null as status, null as meeting_id
+            from modules m
+                inner join schedules s on s.module_id = m.id
+            where m.user_id = teacher_id
+        UNION
+            select "Reunión" as event, m.day as day, m.time as hour, 
+            case
+                when m.user_id = p.user_id then "creator"
+                else "guest"
+            end as type,
+            m.status as status, m.id as meeting_id 
+            from meetings m 
+                inner join participants p on m.id = p.meeting_id 
+            where p.user_id = teacher_id and m.week = selected_week
             order by day asc, hour asc;
-
-            END
+        END
         ');
     }
 
