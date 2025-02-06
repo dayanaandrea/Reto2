f4591e7f07637825b8f19c8506bf10575e1fef97
Dayana <dayana@DESKTOP-91VHDON.>
2025-02-06 02:55:33 +0100

    Se hacen últimos cambios

--- resources/lang/eus/module.php
+++ resources/lang/eus/module.php
@@ -2,9 +2,8 @@
 
 return [
     // INDEX
-    'index_title_1' => 'Modulu berri bat sortu',
-    'index_title_2' => 'Modulua sortzera sartzea:',
-    'module' => 'Moduluak',
+    'create_module' => 'Modulu berri bat sortu',
+    'module' => 'Modulua ',
     'code' => 'Kodea',
     'name' => 'Izena',
     'hours' => 'Orduak',
@@ -13,47 +12,43 @@ return [
     'teacher' => 'Irakaslea',
     'actions' => 'Ekintzak',
     'not_assigned' => 'Ez dago esleiturik',
-    'see_data_module' => 'Ikusi moduluko datuak',
-    'edit_data_module' => 'Editatu moduluko datuak',
-    'delete_module' => 'Ezabatu moduluak',
-    'confirm_deletes' => 'Ezabaltzea berretsi',
-    'confirm_1' => 'Ziur zaude modulua ezabatzea nahi duzula ',
-    'confirm_2' => '? Ekintza hau ezin da desegondu.',
-    'cancel' => 'Ezeztatu',
+    'see_data_module' => 'Moduluaren xehetasunak ikusi',
+    'edit_data_module' => 'Moduluaren datuak aldatu',
+    'delete_module' => 'Modulua ezabatu',
+    'confirm_deletes' => 'Ezabatzea baieztatu',
+    'confirm_1' => 'Ziur al zaude modulua ezabatu nahi duzula ',
+    'confirm_2' => '? Ekintza hau ez da atzera egiteko modukoa.',
+    'cancel' => 'Utzi',
     'delete' => 'Ezabatu',
-    'add' => 'Gehitu',
     'update' => 'Eguneratu',
-    'update_text' => 'Zikloaren Eguneraketa',
+    'update_text' => 'Zikloaren eguneratzea',
     'create' => 'Sortu',
-    'create_text' => 'Zikloaren Sortzea',
+    'create_text' => 'Zikloaren sorrera',
     'add' => 'Gehitu',
 
-
-    // SHOW  {{ $module->name }}
-    'show_title' => 'Moduluko xehetasunak',
+    // SHOW  
+    'show_title' => 'Moduluaren xehetasunak',
 
     // CREATE-EDIT
     'select_teacher' => ' -- Aukeratu irakasle bat -- ',
 
     // CONTROLLER 
 
-    'code_required' => 'Kodearen eremuak derrigorrezkoa da.',
+    'code_required' => 'Kodea derrigorrezkoa da.',
     'code_min' => 'Kodeak gutxienez 2 karaktere izan behar ditu.',
-    'code_max' => 'Kodeak ez du gainditu behar 6 karaktere.',
-    'name_required' => 'Izena eremua derrigorrezkoa da.',
-    'name_min' => 'Izena gutxienez 5 karaktere izan behar ditu.',
-    'name_max' => 'Izena ez du gainditu behar 255 karaktere.',
-    'hours_required' => 'Orduen eremuak derrigorrezkoa da.',
-    'hours_numeric' => 'Sartu orduentzako zenbaki baliodun bat.',
-    'course_in' => 'Kursa bakarrik 1 edo 2 izan daiteke.',
-    'course_required' => 'Kursoko eremua derrigorrezkoa da.',
-    'cycle_id_required' => 'Zikloaren eremua derrigorrezkoa da.',
+    'code_max' => 'Kodeak ezingo du 6 karaktere baino gehiago izan.',
+    'name_required' => 'Izena derrigorrezkoa da.',
+    'name_min' => 'Izena gutxienez 5 karaktere izan behar du.',
+    'name_max' => 'Izena ezingo du 255 karaktere baino gehiago izan.',
+    'hours_required' => 'Orduak derrigorrezkoak dira.',
+    'hours_numeric' => 'Mesedez, sartu baliozko zenbaki bat orduetan.',
+    'course_in' => 'Ikastaroa 1 edo 2 izan daiteke bakarrik.',
+    'course_required' => 'Ikastaroa derrigorrezkoa da.',
+    'cycle_id_required' => 'Zikloa derrigorrezkoa da.',
 
-
-    'controller_create' => ' ondo sortu da.',
-    'controller_edit' => ' ondo aldatua izan da.',
-    'controller_delete' => ' ondo ezabatua izan da.',
+    'controller_create' => ' arrakastaz sortu da.',
+    'controller_edit' => ' arrakastaz aldatu da.',
+    'controller_delete' => ' arrakastaz ezabatu da.',
     'controller_error_create' => ' Moduluaren sorrera errorea izan da.',
     'controller_error_edit' => ' Moduluaren aldaketa errorea izan da.',
-
 ];
